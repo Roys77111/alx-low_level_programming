cdf911fef4797ca1e3514da00144b6331a7d701c
roys77111 <roys77111@gmail.com>
2023-01-16 19:17:18 -0800

    100-operations.so

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef Roys77111
+#define roys77111
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,5 +24,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 #endif /* MAIN_H */
